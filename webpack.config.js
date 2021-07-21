5a4ebd949c91cea54e40a1c5348f76b9b6eea094
kuncc <kuncc@foxmail.com>
2021-07-21 08:52:15 +0800

    添加 webpack externals 配置

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
   entry: {
     index: './lib/index.tsx'
   },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
   output: {
     path: path.resolve(__dirname, 'dist/lib'),
     library: 'xiyoUI',
@@ -24,5 +27,19 @@ module.exports = {
       title: 'xiyo-ui',
       template: 'index.html'
     })
-  ]
+  ],
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM'
+    }
+  }
 }
