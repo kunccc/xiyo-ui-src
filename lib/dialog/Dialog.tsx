406c39c181c5e7572b2d0dd579f08d49d96141fc
kuncc <kuncc@foxmail.com>
2021-07-24 23:08:57 +0800

    Dialog 添加 type props

--- lib/dialog/Dialog.tsx
+++ lib/dialog/Dialog.tsx
@@ -6,13 +6,14 @@ import './dialog.scss';
 interface Props {
   visible: boolean;
   title?: string;
+  type: 'confirm' | 'alert' | 'modal';
   closeOnMask?: boolean;
   onClose: Function;
   onCancel?: Function;
   onConfirm?: Function;
 }
 
-const Dialog: React.FC<Props> = ({onClose, visible, closeOnMask, title, children, onCancel, onConfirm}) => {
+const Dialog: React.FC<Props> = ({onClose, visible, closeOnMask, title, type, children, onCancel, onConfirm}) => {
   return (createPortal(
     <>
       <div className={`xiyo-dialog-mask ${visible ? 'visible' : ''}`}
@@ -22,8 +23,9 @@ const Dialog: React.FC<Props> = ({onClose, visible, closeOnMask, title, children
         <header>{title}</header>
         <main>{children}</main>
         <footer>
-          <Button onClick={() => onCancel && onCancel() || onClose()}>取消</Button>
-          <Button onClick={() => onConfirm && onConfirm() || onClose()} level="main">确定</Button>
+          {type === 'confirm' && <Button onClick={() => onCancel && onCancel() || onClose()}>取消</Button>}
+          {(type === 'confirm' || type === 'alert') &&
+          <Button onClick={() => onConfirm && onConfirm() || onClose()} level="main">确定</Button>}
         </footer>
       </div>
     </>
