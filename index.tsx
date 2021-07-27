a74a618b56b3c33c2dc717dc2101ed11741a4fef
kuncc <kuncc@foxmail.com>
2021-07-27 11:14:39 +0800

    美化页面样式

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as  ReactDOM from 'react-dom';
-import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import {HashRouter as Router, Route, NavLink} from 'react-router-dom';
 import IconExample from './lib/icon/icon.example';
 import DialogExample from './lib/dialog/dialog.example';
 import ButtonExample from './lib/button/button.example';
@@ -13,23 +13,33 @@ import './index.scss';
 
 ReactDOM.render(
   <Router>
-    <Layout>
-      <Header>
-        <img src="./logo.png" alt=""/>
-        <img src="./xiyo.png" alt=""/>
+    <Layout className="site">
+      <Header className="site-header">
+        <img className="logo" src="./logo.png" alt=""/>
+        <img className="xiyo" src="./xiyo.png" alt=""/>
       </Header>
       <Layout>
-        <Aside>
-          <Link to="/icon">Icon</Link>
-          <Link to="/dialog">对话框</Link>
-          <Link to="/button">按钮</Link>
-          <Link to="/layout">布局</Link>
+        <Aside className="site-aside">
+          <ul>
+            <li>
+              <NavLink to="/icon">Icon 图标</NavLink>
+            </li>
+            <li>
+              <NavLink to="/button">Button 按钮</NavLink>
+            </li>
+            <li>
+              <NavLink to="/layout">Layout 布局</NavLink>
+            </li>
+            <li>
+              <NavLink to="/dialog">Dialog 对话框</NavLink>
+            </li>
+          </ul>
         </Aside>
-        <Main>
+        <Main className="site-main">
           <Route path="/icon" component={IconExample}/>
-          <Route path="/dialog" component={DialogExample}/>
           <Route path="/button" component={ButtonExample}/>
           <Route path="/layout" component={LayoutExample}/>
+          <Route path="/dialog" component={DialogExample}/>
         </Main>
       </Layout>
     </Layout>
